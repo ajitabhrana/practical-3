379d732dfbf0b609da0626c3df2a9230b712baca
Ajitabh Rana <ajitabhrana@gmail.com>
2020-11-01 11:09:50 +0530

    Update prompt.js

--- prompt.js
+++ prompt.js
@@ -1,6 +1,6 @@
 var name=prompt("Enter your name");
 if(name!=null&&name!=""&&Cookies.get('name')!=name){
-  Cookies.set('name',name{ expires: 1 })
+  Cookies.set('name',name,{ expires: 1 })
   alert("Welcome "+Cookies.get('name'));
 }else{
   alert("Welcome again "+Cookies.get('name'))
